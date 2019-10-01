1f4e36e1298738c5dd7de45952538caeba5f4962
Josemberg Ferreira <josembergff@gmail.com>
2019-10-01 13:08:42 -0300

    ajustes de post e get

--- SimpleClientWebApiVB/ClientWebApiJwtVB.vb
+++ SimpleClientWebApiVB/ClientWebApiJwtVB.vb
@@ -1,37 +1,69 @@
-Imports SimpleClientWebApi
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports Newtonsoft.Json
+Imports System.Text
+
 
 Public Class ClientWebApiJwtVB
     Public NotificationsMessage As String
     Public Notifications As String()
+    Private _uriWebApi, _keyJwt, _typeKey As String
+    Private _client As HttpClient
 
-    Public Function GetApi(Of T1 As New)(ByVal chave As String, ByVal uri As String) As T1
+    Public Sub New(ByVal keyJwt As String, ByVal uriWebApi As String, Optional ByVal typeKey As String = "")
+        _keyJwt = keyJwt
+        _uriWebApi = uriWebApi
+        _client = New HttpClient()
+        _typeKey = typeKey
 
-        Dim client As ClientWebApiJwt = New ClientWebApiJwt(chave, uri)
-
-        Dim response As T1 = client.Get(Of T1, Object)()
-
-        getNotifications(client)
-
-        Return response
-    End Function
-
-    Public Function PostApi(Of T1 As New, T2 As New)(ByVal chave As String, ByVal uri As String, ByRef send As T2) As T1
-
-        Dim client As ClientWebApiJwt = New ClientWebApiJwt(chave, uri)
-
-        Dim response As T1 = client.Post(Of T1, T2)(send)
-
-        getNotifications(client)
-
-        Return response
-
-    End Function
-
-    Private Sub getNotifications(ByVal client As ClientWebApiJwt)
-        If Not client.IsValid() Then
-            NotificationsMessage = client.NotificationsMessage
-            Notifications = client.ListNotifications
+        If String.IsNullOrEmpty(typeKey) Then
+            _typeKey = "Bearer"
         End If
+
+        _client.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue(_typeKey, keyJwt)
     End Sub
+
+
+    Public Function GetApi(Of T1 As New)(Optional ByVal route As String = "") As T1
+        Dim valueReturn As T1 = New T1()
+        Dim urlApi = _uriWebApi
+
+        If Not String.IsNullOrEmpty(route) Then
+            urlApi = _uriWebApi + "/" + route
+        End If
+
+        Dim result = _client.GetAsync(urlApi).Result
+
+        If result.StatusCode = System.Net.HttpStatusCode.OK Then
+            Dim reponseClient = result.Content.ReadAsStringAsync().Result
+            valueReturn = JsonConvert.DeserializeObject(Of T1)(reponseClient)
+        End If
+
+        Return valueReturn
+
+    End Function
+
+    Public Function PostApi(Of T1 As New, T2 As New)(ByVal objectSend As T2, Optional ByVal route As String = "") As T1
+
+        Dim valueReturn As T1 = New T1()
+        Dim urlApi = _uriWebApi
+
+        If Not String.IsNullOrEmpty(route) Then
+            urlApi = _uriWebApi + "/" + route
+        End If
+        Dim serializedEmailContract = JsonConvert.SerializeObject(objectSend)
+
+        Dim content = New StringContent(serializedEmailContract, Encoding.UTF8, "application/json")
+        Dim result = _client.PostAsync(urlApi, content).Result
+
+        If result.StatusCode = System.Net.HttpStatusCode.OK Then
+            Dim reponseClient = result.Content.ReadAsStringAsync().Result
+            valueReturn = JsonConvert.DeserializeObject(Of T1)(reponseClient)
+        End If
+
+        Return valueReturn
+
+    End Function
+
 
 End Class
